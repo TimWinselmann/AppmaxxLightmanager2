1d714579965244b634e0f24432e528511a9bae49
Tim Winselmann <twinselmann@googlemail.com>
2017-10-01 20:51:12 +0200

    add user presences + mqtt support

--- js/mqtt.js
+++ js/mqtt.js
@@ -1,36 +1,123 @@
-// Create a client instance
-client = new Paho.MQTT.Client("ws://10.255.0.160:1884/", "BXTestlaptop");
+app.factory("MQTTService", function($rootScope, $log, $interval) {
 
-// set callback handlers
-client.onConnectionLost = onConnectionLost;
-client.onMessageArrived = onMessageArrived;
+    var callbacks = [];
 
-// connect the client
-client.connect({
-    onSuccess: onConnect
+    $rootScope.mqttConnected = false;
+
+    // Create a client instance
+    //
+    client = new Paho.MQTT.Client("ws://192.168.178.49:1884/", "web-mqtt-client");
+
+    // set callback handlers
+    client.onConnectionLost = onConnectionLost;
+    client.onMessageArrived = onMessageArrived;
+
+    // connect the client
+    client.connect({
+        onSuccess: function() {
+            $log.debug('MQTT connected!');
+
+            $rootScope.$apply(function() {
+                $rootScope.mqttConnected = true;
+            });
+
+            /* subscribe to already done callbacks */
+            $log.debug('Subscribe to already saved callbacks.');
+            angular.forEach(callbacks, function(callback, i) {
+                $log.debug('Subscribing to topic >' + callback.topic + '<');
+                client.subscribe(callback.topic);
+            });
+
+        }
+    });
+
+    function onConnectionLost(responseObject) {
+        if (responseObject.errorCode !== 0) {
+            $log.error("onConnectionLost:" + responseObject.errorMessage);
+            $rootScope.$apply(function() {
+                $rootScope.mqttConnected = false;
+            });
+        }
+    };
+
+    function onMessageArrived(message) {
+        var topic = message.destinationName;
+        var payload = message.payloadString;
+
+        $log.debug('onMessageArrived: ' + topic + ' ' + message);
+
+        angular.forEach(callbacks, function(callback, i) {
+            // callback.callback;
+            // callback.topic;
+
+            var regexpStr = callback.topic.replace(new RegExp('(#)|(\\*)|(\\+)'), function(str) {
+                switch (str) {
+                    case "#":
+                        return ".*?"
+                        break;
+                    case "*":
+                        return ".*?"
+                        break;
+                    case "+":
+                        return ".*"
+                        break;
+                    default:
+                        break;
+                }
+            });
+
+            if (topic.match(regexpStr)) {
+                $rootScope.$apply(function() {
+
+                    /* TODO TW let mqtt connected icon blink */
+
+                    callback.callback(payload);
+                });
+            }
+
+        });
+
+    };
+
+    this.send = function(topic, message) {
+        if (!client.isConnected()) {
+            $log.error('Publish MQTT Message not possible: not connected!');
+            return;
+        }
+
+        $log.debug('send: ' + topic + ' ' + message);
+
+        mqttMessage = new Paho.MQTT.Message(message);
+        mqttMessage.destinationName = topic;
+
+        client.send(mqttMessage);
+    };
+
+    this.on = function(topic, callback) {
+        callbacks.push({
+            'topic': topic,
+            'callback': callback
+        });
+
+        if (client.isConnected()) {
+            $log.debug('Subscribing to topic >' + topic + '<');
+            client.subscribe(topic);
+        } else {
+            $log.warn('MQTT not yet connected, saving subscription on >' + topic + '< for later');
+        }
+    };
+
+    this.drop = function(topic) {
+        callbacks[topic] = undefined;
+
+        if (client.isConnected()) {
+            $log.debug('unsubscribe: ' + topic);
+            client.unsubscribe(topic);
+        } else {
+            $log.error('drop: MQTT not connected!');
+        }
+    };
+
+    return this;
+
 });
-
-
-// called when the client connects
-function onConnect() {
-    // Once a connection has been made, make a subscription and send a message.
-    console.log("onConnect");
-    client.subscribe("devices/+/#");
-    message = new Paho.MQTT.Message("Hello");
-    message.destinationName = "devices/laptop/text";
-    client.send(message);
-}
-
-// called when the client loses its connection
-function onConnectionLost(responseObject) {
-    if (responseObject.errorCode !== 0) {
-        console.log("onConnectionLost:" + responseObject.errorMessage);
-    }
-}
-
-// called when a message arrives
-function onMessageArrived(message) {
-    Materialize.toast(message.payloadString, 4000);
-    console.log(message.destinationName);
-    [group, device, sensorType] = message.destinationName.split("/");
-}
---
+++ js/testCtrl.js
@@ -0,0 +1,108 @@
+app.controller("TestCtrl", function($scope, $http, $log, $interval, MQTTService) {
+
+    $scope.title = 'Home';
+
+    $scope.loading = false;
+    /*
+    $interval(function() {
+        $scope.loading = false;
+        //$('.card-content').matchHeight();
+    }, 1000);
+    */
+
+    $scope.publishMqttMessage = function() {
+        MQTTService.send('Hello', 'World');
+    }
+
+    MQTTService.on('Hello', function(message) {
+        Materialize.toast('Incoming MQTT Message: ' + message, 4000);
+    });
+
+    $scope.toggleLight = function(room, light) {
+        if (light.checked) {
+            Materialize.toast(room.name + ' ' + light.name + ' wurde eingeschaltet', 4000);
+        } else {
+            Materialize.toast(room.name + ' ' + light.name + ' wurde ausgeschaltet', 4000);
+        }
+    }
+
+    $scope.users = [{
+        'name': 'Tim',
+        'home': true
+    }, {
+        'name': 'Josie',
+        'home': false
+    }, {
+        'name': 'Ivy',
+        'home': true
+    }];
+
+    $scope.rooms = [{
+        'id': 1,
+        'name': 'Wohnzimmer',
+        'icon': 'weekend',
+        'temperature': 21.6,
+        'lights': [{
+            'id': 1,
+            'name': 'Esstisch',
+            'checked': true
+        }, {
+            'id': 2,
+            'name': 'Regal',
+            'checked': false
+        }, {
+            'id': 7,
+            'name': 'Ambilight',
+            'checked': false
+        }]
+    }, {
+        'id': 2,
+        'name': 'Flur',
+        'icon': 'map',
+        'temperature': 22.4,
+        'lights': [{
+            'id': 3,
+            'name': 'Eingang',
+            'checked': true
+        }, {
+            'id': 4,
+            'name': 'Kommode',
+            'checked': true
+        }]
+    }, {
+        'id': 3,
+        'name': 'K\u00fcche',
+        'icon': 'kitchen',
+        'temperature': 26.35,
+        'lights': [{
+            'id': 5,
+            'name': 'Sp\u00fcle',
+            'checked': false
+        }, {
+            'id': 6,
+            'name': 'Regal',
+            'checked': false
+        }]
+    }, {
+        'id': 4,
+        'name': 'Bad',
+        'icon': 'wc',
+        'temperature': 22.35,
+        'lights': [{
+            'id': 15,
+            'name': 'Spots',
+            'checked': false
+        }]
+    }, {
+        'id': 5,
+        'name': 'Kino',
+        'icon': 'videocam',
+        'temperature': 21.0,
+        'lights': [{
+            'id': 16,
+            'name': 'Spots',
+            'checked': false
+        }]
+    }];
+
+});
